1ac518dc545b9a829111c04d0aaf8cbd1eb75023
MKmanojanand <manojanand118@gmail.com>
2025-12-29 10:49:16 +0530

    kumkum

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import express from "express";
 import fetch from "node-fetch";
-import cors from "cors";
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
